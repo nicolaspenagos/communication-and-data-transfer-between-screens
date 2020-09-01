79c170166c499a5b554009e289b2fbd1edc7977e
Nicolas Penagos Montoya <nicolas.penagosm98@gmail.com>
2020-09-01 00:23:39 -0500

    SettingsColorActivity's skeleton has been coded.

---
+++ app/src/main/java/com/example/communication_and_data_transfer_between_screens/ColorSettings.java
@@ -0,0 +1,13 @@
+package com.example.communication_and_data_transfer_between_screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ColorSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_color);
+    }
+}
