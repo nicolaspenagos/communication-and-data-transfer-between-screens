86b88f605587b42d853a598c39e74ab3a2c202f6
Nicolas Penagos Montoya <nicolas.penagosm98@gmail.com>
2020-08-31 23:49:19 -0500

    Initial commit

---
+++ app/src/main/java/com/example/communication_and_data_transfer_between_screens/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.communication_and_data_transfer_between_screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/com/example/communication_and_data_transfer_between_screens/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.communication_and_data_transfer_between_screens;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
