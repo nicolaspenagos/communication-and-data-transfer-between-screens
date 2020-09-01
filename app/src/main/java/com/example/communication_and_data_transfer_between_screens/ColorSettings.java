169a56842271bad8e3fce2174d198cccc5a5aa73
Nicolas Penagos Montoya <nicolas.penagosm98@gmail.com>
2020-09-01 00:28:45 -0500

    References have been coded.

--- app/src/main/java/com/example/communication_and_data_transfer_between_screens/ColorSettings.java
+++ app/src/main/java/com/example/communication_and_data_transfer_between_screens/ColorSettings.java
@@ -1,13 +1,32 @@
+/**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * @author Nicolás Penagos Montoya
+ * nicolas.penagosm98@gmail.com
+ **~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
 package com.example.communication_and_data_transfer_between_screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ColorSettings extends AppCompatActivity {
 
+    // -------------------------------------
+    // XML references
+    // -------------------------------------
+    private Button blueButton;
+    private Button whiteButton;
+    private Button blackButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_color);
+
+        blueButton = findViewById(R.id.blueButton);
+        whiteButton = findViewById(R.id.whiteButton);
+        blackButton = findViewById(R.id.blackButton);
+
     }
 }
