58e8ba52e0d744fd5a3daf699583f2af4a3765ba
Nicolas Penagos Montoya <nicolas.penagosm98@gmail.com>
2020-08-31 23:57:32 -0500

    Main Activity skeleton has been coded.

--- app/src/main/java/com/example/communication_and_data_transfer_between_screens/MainActivity.java
+++ app/src/main/java/com/example/communication_and_data_transfer_between_screens/MainActivity.java
@@ -1,14 +1,40 @@
+/**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * @author Nicolás Penagos Montoya
+ * nicolas.penagosm98@gmail.com
+ **~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
 package com.example.communication_and_data_transfer_between_screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    // -------------------------------------
+    // XML references
+    // -------------------------------------
+    private Button settingsButton;
+    private TextView nameTextView;
+    private EditText nameEditText;
+    private Button nextButton;
+
+    // -------------------------------------
+    // Gui methods
+    // -------------------------------------
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        settingsButton = findViewById(R.id.settingsButton);
+        nameTextView = findViewById(R.id.nameTextView);
+        nameEditText = findViewById(R.id.nameEditText);
+        nextButton = findViewById(R.id.nextButton);
+
     }
+    
 }
