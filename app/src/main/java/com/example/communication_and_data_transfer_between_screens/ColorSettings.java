6859585fd1fdaca6661327642db5abd243c74f95
Nicolas Penagos Montoya <nicolas.penagosm98@gmail.com>
2020-09-03 00:27:27 -0500

    Color settings capability has been implemented and is working properly.

--- app/src/main/java/com/example/communication_and_data_transfer_between_screens/ColorSettings.java
+++ app/src/main/java/com/example/communication_and_data_transfer_between_screens/ColorSettings.java
@@ -6,10 +6,20 @@
 package com.example.communication_and_data_transfer_between_screens;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-public class ColorSettings extends AppCompatActivity {
+public class ColorSettings extends AppCompatActivity implements View.OnClickListener{
+
+    // -------------------------------------
+    // Color Constants
+    // -------------------------------------
+    public final static char BLUE = 'B';
+    public final static char WHITE = 'W';
+    public final static char BLACK ='K';
 
     // -------------------------------------
     // XML references
@@ -27,6 +37,43 @@ public class ColorSettings extends AppCompatActivity {
         blueButton = findViewById(R.id.blueButton);
         whiteButton = findViewById(R.id.whiteButton);
         blackButton = findViewById(R.id.blackButton);
+
+        blueButton.setOnClickListener(this);
+        whiteButton.setOnClickListener(this);
+        blackButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        Intent i = new Intent();
+
+        switch (view.getId()){
+
+            case R.id.blueButton:
+
+                i.putExtra("color", BLUE);
+                setResult(RESULT_OK, i);
+
+                break;
+
+            case R.id.whiteButton:
+
+                i.putExtra("color", WHITE);
+                setResult(RESULT_OK, i);
+
+                break;
+
+            case R.id.blackButton:
+
+                i.putExtra("color", BLACK);
+                setResult(RESULT_OK, i);
+                break;
+
+        }
+
+        finish();
 
     }
 }
--- app/src/main/java/com/example/communication_and_data_transfer_between_screens/MainActivity.java
+++ app/src/main/java/com/example/communication_and_data_transfer_between_screens/MainActivity.java
@@ -5,8 +5,12 @@
  */
 package com.example.communication_and_data_transfer_between_screens;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     // -------------------------------------
     // XML references
     // -------------------------------------
+    private ConstraintLayout mainLayout;
     private Button settingsButton;
     private TextView nameTextView;
     private EditText nameEditText;
@@ -31,11 +36,77 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mainLayout = findViewById(R.id.mainLayout);
         settingsButton = findViewById(R.id.settingsButton);
         nameTextView = findViewById(R.id.nameTextView);
         nameEditText = findViewById(R.id.nameEditText);
         nextButton = findViewById(R.id.nextButton);
 
+        settingsButton.setOnClickListener(
+
+                (view)->{
+
+                    Intent i = new Intent(this, ColorSettings.class);
+                    startActivityForResult(i, 101);
+
+                }
+        );
+
+        darkMode(false);
+
     }
-    
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 101 && resultCode == RESULT_OK){
+
+            char color = data.getExtras().getChar("color");
+            switch (color) {
+
+                case ColorSettings.BLUE:
+
+                    mainLayout.setBackgroundColor(Color.rgb(142, 211, 233));
+                    darkMode(false);
+
+                    break;
+
+                case ColorSettings.WHITE:
+
+                    mainLayout.setBackgroundColor(Color.rgb(255, 255, 255));
+                    darkMode(false);
+
+                    break;
+
+                case ColorSettings.BLACK:
+
+                    mainLayout.setBackgroundColor(Color.rgb(38, 38, 38));
+                    darkMode(true);
+
+                    break;
+            }
+
+        }
+
+    }
+
+    public void darkMode(boolean darkMode){
+
+        if(darkMode){
+
+            nameTextView.setTextColor(Color.rgb(255,255,255));
+            nameEditText.setTextColor(Color.rgb(255,255,255));
+            nameEditText.setHintTextColor(Color.rgb(180,180,180));
+
+        }else{
+
+            nameTextView.setTextColor(Color.rgb(0,0,0));
+            nameEditText.setTextColor(Color.rgb(0,0,0));
+            nameEditText.setHintTextColor(Color.rgb(120,120,120));
+
+
+        }
+
+    }
 }
